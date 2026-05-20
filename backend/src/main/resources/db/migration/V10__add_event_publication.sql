0173590d77bcf5e96c7403a946e80c239c4a1bb1
David Gilligan <archergilly84@gmail.com>
2026-05-20 23:42:37 +0100

    Added migration to fix missing event_publication that is required by spring modulith

---
+++ backend/src/main/resources/db/migration/V10__add_event_publication.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS event_publication (
+                                                 id               UUID                        NOT NULL,
+                                                 listener_id      TEXT                        NOT NULL,
+                                                 event_type       TEXT                        NOT NULL,
+                                                 serialized_event TEXT                        NOT NULL,
+                                                 publication_date TIMESTAMP WITH TIME ZONE    NOT NULL,
+                                                 completion_date  TIMESTAMP WITH TIME ZONE,
+                                                 PRIMARY KEY (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_event_publication_incomplete
+    ON event_publication (listener_id, completion_date)
+    WHERE completion_date IS NULL;
